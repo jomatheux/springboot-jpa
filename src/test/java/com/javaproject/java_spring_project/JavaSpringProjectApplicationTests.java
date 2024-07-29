d4aa62d2cb965c2f51293a19213c2d1708387a5b
jomatheux <jomatheux@gmail.com>
2024-07-29 11:27:14 -0300

    Project created

---
+++ src/test/java/com/javaproject/java_spring_project/JavaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.java_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
