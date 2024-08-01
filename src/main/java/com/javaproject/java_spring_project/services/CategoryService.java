7bb7f19b1d676b012204845f980776494aafed91
jomatheux <jomatheux@gmail.com>
2024-08-01 10:22:06 -0300

    Category

---
+++ src/main/java/com/javaproject/java_spring_project/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.javaproject.java_spring_project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaproject.java_spring_project.entities.Category;
+import com.javaproject.java_spring_project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
